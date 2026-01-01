b3deca61333dd6a94b182ec3d5a76987942f55a6
Sandeep Singh Chouhan <sandeepchouhan.6261@gmail.com>
2026-01-01 11:19:50 +0530

    Create QuotePage.jsx

---
+++ src/pages/QuotePage.jsx
@@ -0,0 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import QuoteCard from "../components/cards/QuoteCard"; 
+
+const QuotePage = () => {
+  const { id } = useParams();
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/quote/${id}`
+        );
+        const data = await res.json();
+        setQuote(data);
+      } catch (err) {
+        console.error("Failed to load quote", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuote();
+  }, [id]);
+
+  if (loading) return <p style={{ textAlign: "center" }}>Loading...</p>;
+  if (!quote) return <p>Quote not found</p>;
+
+  return (
+    <div style={{ maxWidth: "500px", margin: "auto" }}>
+      <QuoteCard quote={quote} />
+    </div>
+  );
+};
+
+export default QuotePage;

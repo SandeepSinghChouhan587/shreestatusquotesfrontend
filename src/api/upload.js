2d026381c07b2cab7593e4c5c5748d78cc650cb1
Sandeep Singh Chouhan <sandeepchouhan.6261@gmail.com>
2026-01-03 11:34:20 +0530

    Update upload.js

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export const getUploadUrl = async (fileType) => {
-  const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/upload/upload-url`, {
-    fileType,
-  });
+export const getUploadUrl = async (data) => {
+  const res = await axios.post(
+    `${import.meta.env.VITE_API_URL}/api/upload/upload-url`,
+    data
+  );
   return res.data;
 };
